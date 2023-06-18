525617a0d383b38550b8a0ce21c4df5de752409a
Sanchous <axanutin@mail.ru>
2023-06-18 12:15:32 +0300

    adding fields

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,10 +5,25 @@ data class Post(
     val createdBy: Int = 0,
     val date: Int = 0,
     val text: String = "text",
-    val postType: String = "postType",
-    val isPinned: Int = 0,
+    val replyOwnerId: Int = 0,
+    val replyPostId: Int = 0,
+    val friendsOnly: Int = 0,
+    val comments: Comments = Comments(),
+    val copyright: Copyright = Copyright(1),
+    val reposts: Reposts = Reposts(),
+    val views: Views = Views(),
+    val postType: String = "post",
+    val postSource: PostSource = PostSource(),
+    val geo: Geo = Geo(),
+    val signerId: Int = 9,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = true,
+    val canEdit: Boolean = true,
+    val isPinned: Boolean = true,
     val markedAsAds: Boolean = true,
     val isFavorite: Boolean = true,
+    val donut: Donut = Donut(),
+    val postponedId: Int = 11,
     val original: Post?,
     val likes: Likes = Likes(),
     val attachment: Array<Attachment> = arrayOf(
@@ -18,8 +33,69 @@ data class Post(
     )
 )
 
-data class Likes(
+data class Donut(
+    val isDonut: Boolean = true,
+    val paidDuration: Int = 10,
+    val canPublishFreeCopy: Boolean = true,
+    val editMode: String = "all"
+)
+
+data class Geo(
+    val type: String = "type",
+    val coordinates: String = "gde-to_tam",
+    val place: Place = Place(0)
+)
+
+data class Place(
+    val id: Int,
+    val title: String = "u_magazine",
+    val latitude: Int = 1,
+    val longitude: Int = 2,
+    val created: Int = 3,
+    val icon: String = "https://icon",
+    val checkins: Int = 4,
+    val updated: Int = 5,
+    val type: Int = 6,
+    val country: Int = 7,
+    val city: Int = 8,
+    val address: String = "Here"
+)
+
+data class PostSource(
+    val type: String = "vk",
+    val platform: String = "iphone",
+    val data: String = "profile_photo",
+    val url: String = "https://m.vk.com/theterminator"
+)
+
+data class Views(
     var count: Int = 0
+)
+
+data class Reposts(
+    var count: Int = 0,
+    val userReposted: Boolean = false
+)
+data class Copyright(
+    val id: Int,
+    val link: String = "link",
+    val name: String = "name",
+    val type: String = "type"
+)
+
+data class Comments(
+    var count: Int = 0,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = true,
+    val canClose: Boolean = true,
+    val canOpen: Boolean = true
+)
+
+data class Likes(
+    var count: Int = 0,
+    val userLikes: Boolean = true,
+    val canLike: Boolean = true,
+    val canPublish: Boolean = true
 )
 
 interface Attachment {
